0680aa9aa7ee23628f25d43ead3138380770fd70
Mark Penny <mark.penny.jm@gmail.com>
2016-04-20 20:38:41 -0400

    CMSC433 Project 0: A multithreaded solution to find the maximum number in a list

---
+++ CMSC433_Parallel_Maximizer/ParallelMaximizerWorker.java
@@ -0,0 +1,47 @@
+package cmsc433.p0;
+
+import java.util.LinkedList;
+
+/**
+ * Given a <code>LinkedList</code>, this class will find the maximum over a
+ * subset of its <code>Integers</code>.
+ */
+public class ParallelMaximizerWorker extends Thread {
+
+	protected LinkedList<Integer> list;
+	protected int partialMax = Integer.MIN_VALUE; // initialize to lowest value
+	
+	public ParallelMaximizerWorker(LinkedList<Integer> list) {
+		this.list = list;
+	}
+	
+	/**
+	 * Update <code>partialMax</code> until the list is exhausted.
+	 */
+	public void run() {
+		while (true) {
+			int number;
+			// check if list is not empty and removes the head
+			// synchronization needed to avoid atomicity violation
+			synchronized(list) {
+				if (list.isEmpty())
+					return; // list is empty
+				number = list.remove();
+			}
+			
+			// update partialMax according to new value
+			if (number > getPartialMax()){
+				setPartialMax(number);
+			}	
+			
+		}
+	}
+	
+	public int getPartialMax() {
+		return partialMax;
+	}
+	private void setPartialMax(int newMax) {
+		partialMax = newMax;
+	}
+
+}
